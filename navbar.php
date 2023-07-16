81de1ccde875859f7715b8deb3eb0300ba664cc2
JV <107622230+hsumyatm7308@users.noreply.github.com>
2023-07-16 20:07:22 +0630

    contact page 16 July

---
+++ navbar.php
@@ -0,0 +1,51 @@
+
+
+
+
+<nav class="navbar navbar-expand-lg    fixed-top ">
+    <a href="" class="navbar-brand d-flex  me-lg-5 ms-lg-5">
+        <img src="./asses/img/fav/perfumlogo.png" alt="" width="50px" class="">
+
+        <h4 class=" text-uppercase  text-light mt-3 fs-5">Perum Dej </h4>
+    </a>
+
+    <button class="navbar-toggler navbuttons " data-bs-toggle="collapse" data-bs-target="#nav"><i
+            class="fa-solid fa-bars"></i></button>
+    <div id="shopcardbtn" class="text-uppercase text-success small ms-lg-5  p-3  shopcardbtn"> <span
+            class="me-1 text-light">Shop</span> <sup id="counts" class="ms-1 fs-6"></sup></div>
+
+
+
+
+    <div id="nav" class="navbar-collapse collapse ">
+
+        <div class=" ms-lg-5 me-lg-5 ms-sm-5">
+            <ul class="navbar-nav text-uppercase small ms-lg-5">
+                <li class="nav-list-item navmenus"><a href="index.php" class="nav-link  p-3">Home</a></li>
+                <li class="nav-list-item navmenus"><a href="#aboutus" class="nav-link  p-3">About Us</a></li>
+                <li class="nav-list-item navmenus"><a href="#products" class="nav-link  p-3">Products</a></li>
+                <li class="nav-list-item navmenus"><a href="#products" class="nav-link  p-3">Blog</a></li>
+                <li class="nav-list-item navmenus"><a href="#property" class="nav-link  p-3">Service</a></li>
+                <li class="nav-list-item navmenus"><a href="contact.php" class="nav-link  p-3">Contact</a></li>
+
+
+                <li class="nav-item  rightside"><a href="javascript:void(0);" class="nav-link p-3">
+                        <input type="search" placeholder="Search..." class="srhbox me-1 ms-lg-5 ms-sm-0"
+                            id="srhbox">
+                        <i id="srhbtn" class="fa-solid fa-magnifying-glass fs-6"></i></a>
+                </li>
+                <li class="nav-list-item"><a href="" class="nav-link text-light p-3"><i
+                            class="fa-solid fa-user"></i></a></li>
+                <li class="nav-list-item loginbtn"><a href="./authanticate/login.php"
+                        class="nav-link text-light p-3">log in</a></li>
+
+            </ul>
+
+
+
+        </div>
+
+
+
+    </div>
+</nav>
